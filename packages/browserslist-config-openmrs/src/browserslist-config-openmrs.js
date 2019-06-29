57861d007788b095135fd88b754562f5641142c6
Joel Denning <joeldenning@gmail.com>
2019-06-29 18:21:12 +0300

    Creating browserslist-config-openmrs package.

---
+++ packages/browserslist-config-openmrs/src/browserslist-config-openmrs.js
@@ -0,0 +1,10 @@
+// See https://github.com/openmrs/openmrs-rfc-frontend/blob/master/text/0003-browser-support.md
+// for how this list was created.
+
+module.exports = [
+  'last 2 Safari major versions',
+  'last 3 Edge major versions',
+  'last 3 Opera major versions',
+  'last 5 Chrome major versions',
+  'last 3 Firefox major versions',
+]
